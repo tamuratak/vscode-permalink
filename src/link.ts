7c1a416795b4a54856034afbf70c5abed3b33841
Takashi Tamura <tamuratak@users.noreply.github.com>
2020-08-28 09:42:45 +0900

    Use vscode.workspace.getWorkspaceFolder(vscode.window.activeTextEditor?.document.uri) to determine a workspace.

--- src/link.ts
+++ src/link.ts
@@ -33,9 +33,16 @@ export class LinkToCode {
         if (dir) {
          return this.findFile(dir)
         }
-		if (!vscode.workspace.workspaceFolders) {
-			return undefined
-		}
+        const curDocUri = vscode.window.activeTextEditor?.document.uri
+        if (curDocUri) {
+            const curFoler = vscode.workspace.getWorkspaceFolder(curDocUri)
+            if (curFoler) {
+                return this.findFile(curFoler)
+            }
+        }
+        if (!vscode.workspace.workspaceFolders) {
+            return undefined
+        }
         for(const folder of vscode.workspace.workspaceFolders) {
             const ret = await this.findFile(folder)
             if (ret) {
@@ -46,7 +53,7 @@ export class LinkToCode {
     }
 
     private async findFile(dir: vscode.WorkspaceFolder) {
-        const uri = vscode.Uri.joinPath(dir.uri, this.path)
+        const uri = vscode.Uri.joinPath(dir.uri, '.', this.path)
         try {
             await vscode.workspace.fs.stat(uri)
             return uri
@@ -80,7 +87,7 @@ export function getLinkAtPosition(document: TextDocument, position: Position): L
         return undefined
     }
     const filePath = match[1]
-    const start = Number(match[2]) - 1
-    const end = match[4] ? Number(match[4]) - 1 : start
+    const start = Number(match[2])
+    const end = match[4] ? Number(match[4]) : start
     return { link: new LinkToCode(filePath, start, end), range }
 }
