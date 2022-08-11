cc5e878667bd232933b23cb104d6b20e226d6e34
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-08-11 13:35:32 +0900

    Edit getFileExt

--- src/utils/fileext.ts
+++ src/utils/fileext.ts
@@ -1,12 +1,10 @@
-import * as path from 'path'
 import * as vscode from 'vscode'
 import type { Permalink } from '../permalink'
 
-export function getFileExt(link: Permalink | vscode.Uri) {
-    const ext = path.posix.extname(link.path)
-    if (ext.startsWith('.')) {
-        return ext.substr(1)
-    } else {
-        return ext
+export function getFileExt(link: Permalink | vscode.Uri): string | undefined {
+    const match = /\.([^.\/]+)$/.exec(link.path)
+    if (match) {
+        return match[1]
     }
+    return
 }
