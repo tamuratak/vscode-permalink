a7ae92bbb3dda69c943f888f32491eedb06ff2d4
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-08-11 18:45:33 +0900

    Remove printCommitHash

--- src/main.ts
+++ src/main.ts
@@ -13,33 +13,6 @@ import type { SnippetArgs, TargetRange } from './types/types'
 
 
 async function printCommitHash() {
-    /*
-    const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports
-	const git = gitExtension?.getAPI(1)
-    const uri = vscode.window.activeTextEditor?.document.uri
-    const workspaceDir = vscode.workspace.workspaceFolders?.[0].uri.fsPath
-    if (workspaceDir && uri) {
-        const revUri = getRevisionUri(workspaceDir, uri.path, '74a5ae3552')
-        console.log(JSON.stringify(revUri))
-        await vscode.commands.executeCommand('vscode.open', revUri)
-        console.log(JSON.stringify(vscode.window.tabGroups.all.map((g) => g.tabs.map((tab) => {
-            const input = tab.input as any
-            const _uri = input.uri as vscode.Uri
-            return _uri.toString()
-        }))))
-        const fileString = await vscode.workspace.fs.readFile(revUri)
-        console.log(fileString.slice(0,100).toString())
-    }
-    if (uri && git) {
-        const dir = path.posix.dirname(uri.path)
-        const repo = await git.init(uri.with({path: dir}))
-        console.log(JSON.stringify(repo?.rootUri.fsPath))
-        let commit = await repo?.getCommit('HEAD')
-        console.log(commit?.hash)
-        commit = await repo?.getCommit('aef51adb2dba')
-        console.log(commit?.hash)
-    }
-    */
     const wspUri = vscode.Uri.parse('workspace://uu@aaa/sr!"#$%&\'@[{`]:;l}*+_?>_/.()0=~|c/main.ts?c=abd123', true)
     console.log(wspUri.toJSON())
     console.log(wspUri.toString())
--- src/utils/fileext.ts
+++ src/utils/fileext.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode'
 import type { Permalink } from '../permalink'
 
 export function getFileExt(link: Permalink | vscode.Uri): string | undefined {
-    const match = /\.([^.\/]+)$/.exec(link.path)
+    const match = /\.([^./]+)$/.exec(link.path)
     if (match) {
         return match[1]
     }
